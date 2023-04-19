114de10a331885772ce410233c27b87cb40dc9f7
Eric-Foreman <131297318+Eric-Foreman@users.noreply.github.com>
2023-04-19 15:21:23 -0400

    Create models.sql

--- models.sql
+++ models.sql
@@ -5,19 +5,19 @@ CREATE TABLE parkEvents (
     event_end_date DATE NOT NULL, 
     event_description VARCHAR(400) NOT NULL); 
 
-CREATE TABLE parkLocations ( 
+CREATE TABLE Locations ( 
     section_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     section_name VARCHAR(50) NOT NULL UNIQUE); 
 
-CREATE TABLE Buildings (
+CREATE TABLE buildings (
     build_id INT NOT NULL AUTO_INCREMENT,
     max_occupancy INT NOT NULL,
     floor_size_sqft INT NOT NULL,
     section_id INT NOT NULL,
     PRIMARY KEY (build_id),
-    FOREIGN KEY (section_id) REFERENCES Park_Locations(section_id));
+    FOREIGN KEY (section_id) REFERENCES parkLocations(section_id));
 
-CREATE TABLE Rides (
+CREATE TABLE rides (
     ride_id INT NOT NULL AUTO_INCREMENT,
     ride_name VARCHAR(50) NOT NULL,
     ride_type VARCHAR(50),
@@ -25,17 +25,18 @@ CREATE TABLE Rides (
     ride_close time NOT NULL,
     max_passengers INT NOT NULL,
     build_id INT,
+    section_id INT NOT NULL,
     PRIMARY KEY (ride_id),
-    FOREIGN KEY (build_id) REFERENCES Park_Locations(section_id),
-    FOREIGN KEY (section_id) REFERENCES Buildings(build_id));
+    FOREIGN KEY (build_id) REFERENCES parkLocations(section_id),
+    FOREIGN KEY (section_id) REFERENCES buildings(build_id));
 
-CREATE TABLE Rides_passenger_restrictions (
+CREATE TABLE passengerRestrictions (
     ride_restrictions VARCHAR(50) NOT NULL,
     ride_id INT,
     PRIMARY KEY (ride_restrictions),
-    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id));
+    FOREIGN KEY (ride_id) REFERENCES rides(ride_id));
     
-CREATE TABLE consessions
+CREATE TABLE parkConsessions
 (
     cons_id	INT			PRIMARY KEY		AUTO_INCREMENT,
     cons_company_name	VARCHAR(50),
@@ -44,51 +45,60 @@ CREATE TABLE consessions
     cons_close 			TIME			NOT NULL, 
     cons_name 			VARCHAR(50)		NOT NULL,
     build_id 			INT,
-    section_id 			INT			NOT NULL,
+    section_id 			INT				NOT NULL,
 	FOREIGN KEY (build_id) REFERENCES
-    	buildings(build_id),
-    	FOREIGN KEY (section_id) REFERENCES
+    	Buildings(build_id),
+    FOREIGN KEY (section_id) REFERENCES
     	parkLocations(section_id)
 	);
     
-CREATE TABLE operate (
+CREATE TABLE employees_rides_operates  (
     ride_id INT NOT NULL,
     emp_id INT NOT NULL,
-    PRIMARY KEY (ride_id),
-    PRIMARY KEY (emp_id),
-    FOREIGN KEY (ride_id) REFERENCES Rides(ride_id),
-    FOREIGN KEY (emp_id) REFERENCES Employees(emp_id));
+    PRIMARY KEY (ride_id, emp_id),
+    FOREIGN KEY (ride_id) REFERENCES rides(ride_id),
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id));
 
-CREATE TABLE operate (
+CREATE TABLE employees_concessions_operate (
     cons_id INT NOT NULL,
     emp_id INT NOT NULL,
-    PRIMARY KEY (cons_id),
-    PRIMARY KEY (emp_id),
-    FOREIGN KEY (cons_id) REFERENCES Concessions(cons_id),
-    FOREIGN KEY (emp_id) REFERENCES Employees(emp_id));
+    PRIMARY KEY (cons_id, emp_id),
+    FOREIGN KEY (cons_id) REFERENCES concessions(cons_id),
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id));
 
-CREATE TABLE host (
+CREATE TABLE employees_parkEvents_host  (
     emp_id INT NOT NULL,
     event_id INT NOT NULL,
     PRIMARY KEY (emp_id),
     PRIMARY KEY (event_id),
-    FOREIGN KEY (emp_id) REFERENCES Employees(emp_id),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id));
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id),
+    FOREIGN KEY (event_id) REFERENCES parkEvents(event_id));
 
-CREATE TABLE resides (
+CREATE TABLE parkEvents_parkLocations_resides (
     section_id INT NOT NULL,
     event_id INT NOT NULL,
-    PRIMARY KEY (section_id),
-    PRIMARY KEY (event_id),
-    FOREIGN KEY (section_id) REFERENCES Park_Locations(section_id),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id));
+    PRIMARY KEY (section_id, event_id),
+    FOREIGN KEY (section_id) REFERENCES parkLocations(section_id),
+    FOREIGN KEY (event_id) REFERENCES parkEvents(event_id));
 
-CREATE TABLE occure_at (
+CREATE TABLE parkEvents_buildings_occureAt_at (
     build_id INT NOT NULL,
     event_id INT NOT NULL,
-    PRIMARY KEY (build_id),
-    PRIMARY KEY (event_id),
-    FOREIGN KEY (build_id) REFERENCES Buildings(build_id),
-    FOREIGN KEY (event_id) REFERENCES Events(event_id));
+    PRIMARY KEY (build_id, event_id),
+    FOREIGN KEY (build_id) REFERENCES buildings(build_id),
+    FOREIGN KEY (event_id) REFERENCES parkEvents(event_id));
 
-CREATE TABLE employees ( emp_id INT PRIMARY KEY AUTO_INCREMENT, emp_FN varchar(50) not null, emp_LN varchar(50) not null, street_name_num varchar(75) not null, city varchar(30) not null, emp_state varchar(30) not null,zip varchar(10) not null,country varchar(30) not null,primary_phone varchar(10) not null,emp_role varchar(50) not null,wage int, date_hired date not null,email varchar(75) not null,salary int); 
+CREATE TABLE employees_repairs_perform (
+    emp_id INT NOT NULL,
+    rep_num INT NOT NULL,
+    ride_id INT NOT NULL,
+    PRIMARY KEY (emp_id, rep_num, ride_id),
+    FOREIGN KEY (emp_id) REFERENCES employees(emp_id),
+    FOREIGN KEY (rep_num) REFERENCES repairs(rep_num),
+    FOREIGN KEY (ride_id) REFERENCES repairs(ride_id));
+
+CREATE TABLE employees ( emp_id INT PRIMARY KEY AUTO_INCREMENT, emp_FN varchar(50) not null, emp_LN varchar(50) not null, street_name_num varchar(75) not null, city varchar(30) not null, emp_state varchar(30) not null,zip varchar(10) not null,country varchar(30) not null,primary_phone varchar(10) not null,emp_role varchar(50) not null,wage int, date_hired date not null,email varchar(75) not null,salary int);
+
+
+
+Create table repairs(rep_num INT NOT NULL AUTO_INCREMENT, ride_id INT NOT NULL, rep_company_name varchar(50), rep_start_date date NOT NULL, rep_fin_date date NOT NULL, rep_description varchar(400) not null, total_cost int not null, primary key (rep_num , ride_id), foreign key (ride_id) references rides(ride_id))
