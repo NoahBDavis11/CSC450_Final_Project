3c2c28baeb151c33c7b776d2670d1e1623e7fcb2
ajc7186 <82669695+ajc7186@users.noreply.github.com>
2023-04-19 14:52:02 -0400

    Update models.sql

--- models.sql
+++ models.sql
@@ -1,1 +1,3 @@
 CREATE TABLE parkEvents ( event_id INT PRIMARY KEY AUTO_INCREMENT, event_name VARCHAR(50) NOT NULL,event_start_date DATE NOT NULL, event_end_date DATE NOT NULL, event_description VARCHAR(400) NOT NULL); 
+
+CREATE TABLE parkLocations ( section_id INT PRIMARY KEY AUTO_INCREMENT, section_name VARCHAR(50) NOT NULL UNIQUE); 
