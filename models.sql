3c0258d97efbefb3b0d8705cd02428b30fbb697a
ajc7186 <82669695+ajc7186@users.noreply.github.com>
2023-04-19 14:44:55 -0400

    Update models.sql

--- models.sql
+++ models.sql
@@ -1,1 +1,4 @@
 line1
+CREATE TABLE parkEvents ( event_id INT PRIMARY KEY AUTO_INCREMENT, event_start_date DATE NOT NULL, event_end_date DATE NOT NULL, event_description VARCHAR(400) NOT NULL); 
+
+line2
