9f187c048df2c369104374359d5196cb9327eff3
NoahBDavis11 <102634479+NoahBDavis11@users.noreply.github.com>
2023-04-19 14:47:16 -0400

    changed

--- models.sql
+++ models.sql
@@ -1,4 +1,1 @@
-line1
-CREATE TABLE parkEvents ( event_id INT PRIMARY KEY AUTO_INCREMENT, event_start_date DATE NOT NULL, event_end_date DATE NOT NULL, event_description VARCHAR(400) NOT NULL); 
-
-line2
+CREATE TABLE parkEvents ( event_id INT PRIMARY KEY AUTO_INCREMENT, event_name VARCHAR(50) NOT NULL,event_start_date DATE NOT NULL, event_end_date DATE NOT NULL, event_description VARCHAR(400) NOT NULL); 
