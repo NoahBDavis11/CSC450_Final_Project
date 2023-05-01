6afaa88aeedf549fcc6bd44e1512c6d856e778fe
ajc7186 <82669695+ajc7186@users.noreply.github.com>
2023-05-01 15:24:22 -0400

    Update all_events.php

--- site/all_events.php
+++ site/all_events.php
@@ -1,7 +1,10 @@
 <?php
 	ini_set ('error_reporting', 1); //Turns on error reporting - remove once everything works.
 	require_once('../mysqli_config.php'); //Connect to the database
-	$query = 'SELECT event_id, event_name, event_start_date, event_end_date, event_description FROM parkEvents';
+	$query = 'SELECT event_id, event_name, event_start_date, event_end_date, event_description, section_name, build_id, emp_id 
+			FROM parkEvents NATURAL JOIN parkEvents_parkLocations_resides NATURAL JOIN parkLocations 
+			NATURAL JOIN parkEvents_buildings_occureAt_at NATURAL JOIN buildings 
+			NATURAL JOIN employees_parkEvents_host NATURAL JOIN employees';
 	$result = mysqli_query($dbc, $query);
 	//Fetch all rows of result as an associative array
 	if($result)
@@ -29,6 +32,9 @@
 			<th>Start Date</th>
 			<th>End Date</th>
 			<th>Description</th>
+			<th>Section Name</th>
+			<th>Building ID</th>
+			<th>Employee ID</th>
 		</tr>	
 		<?php foreach ($all_rows as $event) {
 			echo "<tr>";
@@ -37,6 +43,9 @@
 			echo "<td>".$event['event_start_date']."</td>";
 			echo "<td>".$event['event_end_date']."</td>";
 			echo "<td>".$event['event_description']."</td>";
+			echo "<td>".$event['section_name']."</td>";
+			echo "<td>".$event['build_id']."</td>";
+			echo "<td>".$event['emp_id']."</td>";
 			echo "</tr>";
 		}
 		?>
