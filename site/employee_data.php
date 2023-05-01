d2d16903a3bd5274773d815848780e0e8c9282b4
NoahBDavis11 <102634479+NoahBDavis11@users.noreply.github.com>
2023-05-01 19:27:48 -0400

    employee lookup

--- site/employee_data.php
+++ site/employee_data.php
@@ -1,10 +1,15 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	require_once '../mysqli_config.php'; // Connect to the database	
+
 	if(!empty($_GET['emp_id'])) {
 		$emp_id = $_GET['emp_id'];
 		
 		require_once('../mysqli_config.php'); //adjust the relative path as necessary to find your config file
 		//Retrieve specific vendor data using prepared statements:
-		$query = "SELECT *, concat(street_name_num," ",city," ",emp_state," ",zip," ", country) as address FROM employees WHERE emp_id = ? order by emp_id";
+		$query = "SELECT *, concat(street_name_num, ' ', city, ' ', emp_state, ' ', zip, ' ', country) as address FROM employees WHERE emp_id = ? order by emp_id";
 		$stmt = mysqli_prepare($dbc, $query);
 		mysqli_stmt_bind_param($stmt, "i", $emp_id);
 		mysqli_stmt_execute($stmt);
@@ -38,12 +43,12 @@
 </head>
 <body>
 	<h2>Employee ID: <?php echo "$emp_id";?></h2>
-	<h3>First Name: <?php echo "$emp_FN";?></h2> 
-	<h3>Last Name: <?php echo "$emp_LN";?></h2> 
-	<h2>Phone: <?php echo "$primary_phone";?></h2>
+	<h2>First Name: <?php echo "$emp_FN";?></h2> 
+	<h2>Last Name: <?php echo "$emp_LN";?></h2> 
+	<h3>Phone: <?php echo "$primary_phone";?></h2>
 	<h3>Role: <?php echo "$emp_role";?></h2> 
 	<h3>Wage: <?php echo "$wage";?></h2> 
-	<h2>Salary: <?php echo "$salary";?></h2>
+	<h3>Salary: <?php echo "$salary";?></h2>
 	<h3>Date Hired: <?php echo "$date_hired";?></h2>
 	<h3>Address: <?php echo "$address";?></h2> 
 	<h3><a href="find_employee.html">Lookup another employee</a></h3>
